b52a10f05b0bf30b7c102fad9114e4e6f5e6d163
shubham <shubhamdeulkar27@gmail.com>
2020-07-20 21:23:38 +0530

    Added: Swagger Implementation

--- Parking-Lot/Startup.cs
+++ Parking-Lot/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Parking_Lot
+namespace ParkingLot
 {
     public class Startup
     {
@@ -26,11 +26,18 @@ namespace Parking_Lot
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
