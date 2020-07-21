f22e69ec78e000c66efcfb5a6b0cfd44d7307165
shubham <shubhamdeulkar27@gmail.com>
2020-07-21 14:36:08 +0530

    Added: Entity Framework Core Implementation

---
+++ RepositoryLayer/DBContext/ParkingLotDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryLayer.DBContext
+{
+    /// <summary>
+    /// Class For DBContext.
+    /// </summary>
+    public class ParkingLotDBContext: DbContext
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="options"></param>
+        public ParkingLotDBContext(DbContextOptions<ParkingLotDBContext> options):base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// DbSet Property For User Model. 
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ RepositoryLayer/Migrations/20200721065556_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Role = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
