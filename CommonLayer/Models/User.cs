c47e43f48b7cd3b91b1ae006ccce18e5250b0fd4
shubham <shubhamdeulkar27@gmail.com>
2020-07-21 10:06:45 +0530

    Added: User Model For Resgistration And Login

---
+++ CommonLayer/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    /// <summary>
+    /// Class For User Model.
+    /// </summary>
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Username Required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Email Id Required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Role Is Required")]
+        [RegularExpression(@"^Admin|^Security$|^Police$|^Driver$", ErrorMessage ="Roles are Admin, Security, Police and Driver")]
+        public string Role { get; set; }
+
+        [Required(ErrorMessage ="Password is Required")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 6,ErrorMessage ="Please Enter Minimum 6 Characters ")]
+        public string Password { get; set; }
+
+    }
+}
