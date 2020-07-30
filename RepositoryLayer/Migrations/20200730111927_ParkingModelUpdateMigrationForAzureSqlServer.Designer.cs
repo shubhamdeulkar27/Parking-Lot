26860d216a68cb1a22b7c6964aac372937afb322
shubham <shubhamdeulkar27@gmail.com>
2020-07-30 21:56:22 +0530

    Added: Migration For Parking Details Update

--- RepositoryLayer/Migrations/20200730111927_ParkingModelUpdateMigrationForAzureSqlServer.Designer.cs
+++ RepositoryLayer/Migrations/20200730111927_ParkingModelUpdateMigrationForAzureSqlServer.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryLayer.DBContext;
 
 namespace RepositoryLayer.Migrations
 {
     [DbContext(typeof(ParkingLotDBContext))]
-    partial class ParkingLotDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200730111927_ParkingModelUpdateMigrationForAzureSqlServer")]
+    partial class ParkingModelUpdateMigrationForAzureSqlServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RepositoryLayer/Migrations/20200730111927_ParkingModelUpdateMigrationForAzureSqlServer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class ParkingModelUpdateMigrationForAzureSqlServer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VehicalOwnerEmail",
+                table: "ParkingDetails",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VehicalOwnerEmail",
+                table: "ParkingDetails");
+        }
+    }
+}
