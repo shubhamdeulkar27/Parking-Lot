376a6653e9698484090aa76fd82435c3c32ef6de
shubham <shubhamdeulkar27@gmail.com>
2020-07-30 16:17:23 +0530

    Added: Established Connection With Azure SQL Server

---
+++ RepositoryLayer/Migrations/20200730104510_InitialMigrationForAzureSqlServer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class InitialMigrationForAzureSqlServer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DriverName",
+                table: "ParkingDetails",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DriverName",
+                table: "ParkingDetails",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
